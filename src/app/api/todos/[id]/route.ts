c40ede8b9eb009ada8df1e0664ca92d8c3ce012f
jeff-gitcode <jeff.gitcode@gmail.com>
2024-05-27 22:03:47 +1000

    updated

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -14,11 +14,69 @@ async function getTodo(id: string) {
     }
 }
 
+async function updateTodo(id: string, req: Request) {
+    try {
+        const response = await fetch(`http://localhost:8090/api/collections/todo/records/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(await req.json()),
+        });
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (e) {
+        console.error(e);
+        throw new Error("Failed to update todos");
+    }
+}
+
+async function deleteTodo(id: string) {
+    try {
+        const response = await fetch(`http://localhost:8090/api/collections/todo/records/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "Accept": 'text/plain',
+                'Content-Type': 'application/json',
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (e) {
+        console.error(e);
+        throw new Error("Failed to delete todos");
+    }
+}
+
 export async function GET(req: Request, { params }: { params: { id: string } }) {
     try {
         const id = params.id;
         const response = await getTodo(id);
         return NextResponse.json(response);
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
+
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+    try {
+        const id = params.id;
+        const response = await updateTodo(id, req);
+        return NextResponse.json(response);
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    try {
+        const id = params.id;
+        const response = await deleteTodo(id);
+        return NextResponse.json("todo deleted");
     } catch (e) {
         console.error(e);
         return NextResponse.json({ error: e.message }, { status: 500 });
