fb3a25c707b762f170e11fd263de499616465e65
jeff-gitcode <jeff.gitcode@gmail.com>
2024-09-26 22:31:54 +1000

    updated

---
+++ src/app/application/todos/todofe.usecase.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from "inversify";
+
+import { TodoDto } from "../dtos/todos/create-todo.input";
+import { ITodoFEUseCase } from "../abstract/todos/itodofe.usecase";
+
+@injectable()
+export class TodoFEUseCase implements ITodoFEUseCase {
+    constructor(
+    ) {
+
+    }
+    GetTodos(): Promise<TodoDto[]> {
+        throw new Error("Method not implemented.");
+    }
+    GetTodoById(): Promise<TodoDto> {
+        throw new Error("Method not implemented.");
+    }
+    UpdateTodoById(): Promise<TodoDto> {
+        throw new Error("Method not implemented.");
+    }
+    DeleteTodoById(): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
+
+    async AddTodo(): Promise<TodoDto> {
+        throw new Error("Method not implemented.");
+    }
+
+}
--- src/app/presentation/applicationProvider.tsx
+++ src/app/presentation/applicationProvider.tsx
@@ -1,29 +1,53 @@
 'use client';
 
-import { createContext, ReactNode } from 'react';
-import React from 'react';
-import { Container } from 'inversify';
+// import { createContext } from 'react';
 
-interface ContextValue {
-    container: Container | null;
-}
+// export const MyContext = createContext("");
 
-const ApplicationContext = createContext<ContextValue>({} as ContextValue);
 
-export const useApplication = (): ContextValue => {
-    return React.useContext(ApplicationContext);
-};
+import React, { useContext, ReactNode } from "react";
 
+import { Container, interfaces } from "inversify";
+
+const ApplicationContext = React.createContext<{ container: Container | null }>({ container: null });
+
+/**
+ * @todo inline component Props
+ */
 type Props = {
     children: ReactNode;
     container: Container;
-}
+};
 
-export const ApplicationProvider: React.FC<Props> = (props: Props) => {
-    return (<ApplicationContext.Provider
-        value={{
-            container: props.container,
-        }}>
-        {props.children}
-    </ApplicationContext.Provider>);
-}
+export const ApplicationProvider: React.FC<Props> = (props) => {
+    return <ApplicationContext.Provider value={{ container: props.container }}>{props.children}</ApplicationContext.Provider>;
+};
+
+
+// import { createContext, ReactNode } from 'react';
+// import React from 'react';
+// import { Container } from 'inversify';
+
+// interface ContextValue {
+//     container: Container | null;
+// }
+
+// const ApplicationContext = createContext<ContextValue>({} as ContextValue);
+
+// export const useApplication = (): ContextValue => {
+//     return React.useContext(ApplicationContext);
+// };
+
+// type Props = {
+//     children: ReactNode;
+//     container: Container;
+// }
+
+// export const ApplicationProvider: React.FC<Props> = (props: Props) => {
+//     return (<ApplicationContext.Provider
+//         value={{
+//             container: props.container,
+//         }}>
+//         {props.children}
+//     </ApplicationContext.Provider>);
+// }
