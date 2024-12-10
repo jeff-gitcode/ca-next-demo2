9f4e1fa001dee08fff7ea9cc2758f7fae94bfd32
jeff-gitcode <jeff.gitcode@gmail.com>
2024-12-10 21:32:49 +1100

    cleanup storybook

--- src/app/presentation/todos/new/page.stories.tsx
+++ src/app/presentation/todos/new/page.stories.tsx
@@ -8,39 +8,6 @@ import { fn } from '@storybook/test';
 import { Container } from 'inversify';
 
 export const useAppContext = fn(actual.useAppContext).mockName('useAppContext');
-useAppContext.mockReturnValue({
-  container: ApplicationContainer,
-  todoListUseCase: () => ({
-    data: [
-      {
-        id: '1',
-        title: 'test title',
-      },
-    ],
-    isLoading: false,
-    error: '',
-  }),
-  todoUseCase: (id: string) => ({
-    data: { id: id, title: 'test title' },
-    isLoading: false,
-    error: '',
-  }),
-  updateTodoUseCase: () => ({
-    updateData: null,
-    updateTodo: fn(),
-    isUpdating: false,
-  }),
-  createTodoUseCase: () => ({
-    createData: null,
-    createTodo: fn(),
-    isCreating: false,
-  }),
-  deleteTodoUseCase: () => ({
-    deleteData: null,
-    deleteTodo: fn(),
-    isDeleting: false,
-  }),
-});
 
 const meta = {
   title: 'NewTodo',
@@ -55,20 +22,7 @@ const meta = {
   },
   decorators: [
     (Story) => (
-      <ApplicationContext.Provider
-        value={{
-          container: ApplicationContainer,
-          todoListUseCase: useAppContext().todoListUseCase,
-          todoUseCase: (id: string) => ({
-            data: { id: id, title: 'test title' },
-            isLoading: false,
-            error: '',
-          }),
-          createTodoUseCase: useAppContext().createTodoUseCase,
-          updateTodoUseCase: useAppContext().updateTodoUseCase,
-          deleteTodoUseCase: useAppContext().deleteTodoUseCase,
-        }}
-      >
+      <ApplicationContext.Provider value={useAppContext()}>
         {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
         <Story />
       </ApplicationContext.Provider>
@@ -82,30 +36,39 @@ type Story = StoryObj<typeof meta>;
 export const Base: Story = {
   beforeEach: () => {
     // jest.clearAllMocks();
-    // useAppContext.mockReturnValue({
-    //   todoListUseCase: () => ({
-    //     data: [
-    //       {
-    //         id: '1',
-    //         title: 'test title',
-    //         description: 'test description',
-    //         status: 'test status',
-    //       },
-    //     ],
-    //     isLoading: false,
-    //     error: '',
-    //   }),
-    //   todoUseCase: () => ({
-    //     data: { id: '1', title: 'test title' },
-    //     isLoading: false,
-    //     error: '',
-    //   }),
-    //   updateTodoUseCase: () => ({
-    //     updateData: null,
-    //     updateTodo: fn(),
-    //     isUpdating: false,
-    //   }),
-    // });
+    useAppContext.mockReturnValue({
+      container: ApplicationContainer,
+      todoListUseCase: () => ({
+        data: [
+          {
+            id: '1',
+            title: 'test title',
+          },
+        ],
+        isLoading: false,
+        error: '',
+      }),
+      todoUseCase: (id: string) => ({
+        data: { id: id, title: 'test title' },
+        isLoading: false,
+        error: '',
+      }),
+      updateTodoUseCase: () => ({
+        updateData: null,
+        updateTodo: fn(),
+        isUpdating: false,
+      }),
+      createTodoUseCase: () => ({
+        createData: null,
+        createTodo: fn(),
+        isCreating: false,
+      }),
+      deleteTodoUseCase: () => ({
+        deleteData: null,
+        deleteTodo: fn(),
+        isDeleting: false,
+      }),
+    });
   },
   // args: {
   //   params: { id: '1' },
--- src/app/presentation/todos/page.stories.tsx
+++ src/app/presentation/todos/page.stories.tsx
@@ -8,39 +8,6 @@ import { fn } from '@storybook/test';
 import { Container } from 'inversify';
 
 export const useAppContext = fn(actual.useAppContext).mockName('useAppContext');
-useAppContext.mockReturnValue({
-  container: ApplicationContainer,
-  todoListUseCase: () => ({
-    data: [
-      {
-        id: '1',
-        title: 'test title',
-      },
-    ],
-    isLoading: false,
-    error: '',
-  }),
-  todoUseCase: (id: string) => ({
-    data: { id: id, title: 'test title' },
-    isLoading: false,
-    error: '',
-  }),
-  updateTodoUseCase: () => ({
-    updateData: null,
-    updateTodo: fn(),
-    isUpdating: false,
-  }),
-  createTodoUseCase: () => ({
-    createData: null,
-    createTodo: fn(),
-    isCreating: false,
-  }),
-  deleteTodoUseCase: () => ({
-    deleteData: null,
-    deleteTodo: fn(),
-    isDeleting: false,
-  }),
-});
 
 const meta = {
   title: 'TodoListPage',
@@ -55,37 +22,7 @@ const meta = {
   },
   decorators: [
     (Story) => (
-      <ApplicationContext.Provider
-        value={{
-          container: ApplicationContainer,
-          todoListUseCase: () => ({
-            data: [
-              {
-                id: '1',
-                title: 'test title1',
-              },
-              {
-                id: '2',
-                title: 'test title2',
-              },
-            ],
-            isLoading: false,
-            error: '',
-          }),
-          todoUseCase: (id: string) => ({
-            data: { id: id, title: 'test title' },
-            isLoading: false,
-            error: '',
-          }),
-          createTodoUseCase: () => ({
-            createData: undefined,
-            createTodo: undefined,
-            isCreating: false,
-          }),
-          updateTodoUseCase: useAppContext().updateTodoUseCase,
-          deleteTodoUseCase: useAppContext().deleteTodoUseCase,
-        }}
-      >
+      <ApplicationContext.Provider value={useAppContext()}>
         {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
         <Story />
       </ApplicationContext.Provider>
@@ -102,32 +39,42 @@ type Story = StoryObj<typeof meta>;
 export const Base: Story = {
   beforeEach: () => {
     // jest.clearAllMocks();
-    // useAppContext.mockReturnValue({
-    //   todoListUseCase: () => ({
-    //     data: [
-    //       {
-    //         id: '1',
-    //         title: 'test title',
-    //         description: 'test description',
-    //         status: 'test status',
-    //       },
-    //     ],
-    //     isLoading: false,
-    //     error: '',
-    //   }),
-    //   todoUseCase: () => ({
-    //     data: { id: '1', title: 'test title' },
-    //     isLoading: false,
-    //     error: '',
-    //   }),
-    //   updateTodoUseCase: () => ({
-    //     updateData: null,
-    //     updateTodo: fn(),
-    //     isUpdating: false,
-    //   }),
-    // });
+    useAppContext.mockReturnValue({
+      container: ApplicationContainer,
+      todoListUseCase: () => ({
+        data: [
+          {
+            id: '1',
+            title: 'test title1',
+          },
+          {
+            id: '2',
+            title: 'test title2',
+          },
+        ],
+        isLoading: false,
+        error: '',
+      }),
+      todoUseCase: (id: string) => ({
+        data: { id: id, title: 'test title' },
+        isLoading: false,
+        error: '',
+      }),
+      updateTodoUseCase: () => ({
+        updateData: null,
+        updateTodo: fn(),
+        isUpdating: false,
+      }),
+      createTodoUseCase: () => ({
+        createData: null,
+        createTodo: fn(),
+        isCreating: false,
+      }),
+      deleteTodoUseCase: () => ({
+        deleteData: null,
+        deleteTodo: fn(),
+        isDeleting: false,
+      }),
+    });
   },
-  // args: {
-  //   params: { id: '1' },
-  // },
 };
